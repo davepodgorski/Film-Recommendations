83f0d8bacdddf95fad8968872eebf4d82c73f194
davepodgorski <davepodgorski@gmail.com>
2018-11-13 15:46:15 -0500

    exercise_4

---
+++ method_exercises.rb
@@ -0,0 +1,37 @@
+# def double(my_number)
+#   return my_number * 2
+# end
+#
+# puts double(545675473)
+#
+# #--------------
+# def negative?(number)
+#   if number < 0
+#     return false
+#   else return true
+#   end
+# end
+#
+# puts negative?(5)
+
+#_________________________
+
+# def is_even?(num)
+#   if num % 2 != 0
+#   return false
+# else return true
+#   end
+# end
+#
+# puts is_even?(6)
+
+#___________________
+
+def short_word(word)
+  if word.length < 8
+    return false
+  else return true
+  end
+end
+
+puts short_word("alghakjhghjhsfjkghsk")
